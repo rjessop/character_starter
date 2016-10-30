e9c8f84a401fa9ea35dbc7a4a008a735df3e47f4
rjessop <rayjessop@msn.com>
2016-10-29 18:41:26 -0700

    changed the main menu. converted to c++ project

---
+++ Source/Character_starter/Character_starter.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Character_starter.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Character_starter, "Character_starter" );
